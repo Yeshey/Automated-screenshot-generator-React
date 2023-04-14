2218c94c94011ffc8cb3d9e8a35b1de8bbf2cd1a
Yeshey <yesheysangpo@hotmail.com>
2023-04-14 19:31:36 +0100

    login and logout to google

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -1,14 +1,37 @@
 import React, { useState } from "react";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider, GoogleLogin, googleLogout, useGoogleOneTapLogin } from '@react-oauth/google';
 
 function GoogleAuth() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const handleLoginSuccess = (credentialResponse) => {
+    console.log(credentialResponse);
+    setIsLoggedIn(true);
+  };
+
+  const handleLogoutClick = () => {
+    googleLogout();
+    setIsLoggedIn(false);
+  };
 
   return (
-    <div>
-      <span> Google Auth </span>
-    </div>
+    <>
+      <GoogleOAuthProvider clientId="191000969607-dbpi9doi2bct7asm8k2lcej10n1f1ac5.apps.googleusercontent.com">
+        {isLoggedIn ? (
+          <button onClick={handleLogoutClick}>Logout</button>
+        ) : (
+          <GoogleLogin
+            onSuccess={handleLoginSuccess}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+            useOneTap
+          />
+        )}
+      </GoogleOAuthProvider>
+    </>
   );
 }
+
 
 export default GoogleAuth;
