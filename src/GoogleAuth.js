66421552aecd5275ba832241b1f914615b69af87
Yeshey <yesheysangpo@hotmail.com>
2023-04-14 20:00:35 +0100

    added consts with keys that might need

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { GoogleOAuthProvider, GoogleLogin, googleLogout, useGoogleOneTapLogin } from '@react-oauth/google';
 
+const CLIENT_ID = "191000969607-0cgqepb1p3act7mm5bnbqm8mcl0tafbg.apps.googleusercontent.com"
+const CLIENT_SECRET = "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"
+const API_KEY = "AIzaSyCEDoKs0VmaiLNWbG0B6aFIiKcBOak90Is"
+
 function GoogleAuth() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -16,7 +20,7 @@ function GoogleAuth() {
 
   return (
     <>
-      <GoogleOAuthProvider clientId="191000969607-dbpi9doi2bct7asm8k2lcej10n1f1ac5.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId="191000969607-0cgqepb1p3act7mm5bnbqm8mcl0tafbg.apps.googleusercontent.com">
         {isLoggedIn ? (
           <button onClick={handleLogoutClick}>Logout</button>
         ) : (
