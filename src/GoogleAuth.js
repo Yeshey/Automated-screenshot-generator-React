211f7f802b772d929a44e7a5237d6af119cab0d7
Yeshey <yesheysangpo@hotmail.com>
2023-04-16 21:56:18 +0100

    able to upload test file to Drive

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -5,10 +5,12 @@ import jwt_decode from "jwt-decode";
 const CLIENT_ID = "191000969607-0cgqepb1p3act7mm5bnbqm8mcl0tafbg.apps.googleusercontent.com"
 const CLIENT_SECRET = "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"
 const API_KEY = "AIzaSyCEDoKs0VmaiLNWbG0B6aFIiKcBOak90Is"
+const SCOPES = "https://www.googleapis.com/auth/drive";
 
 function GoogleAuth() {
 
   const [user, setUser] = useState({});
+  const [tokenClient, setTokenClient] = useState({});
 
   function handleCallbackResponse(response){
     console.log("Encoded JWT ID token: " + response.credential);
@@ -23,10 +25,15 @@ function GoogleAuth() {
     document.getElementById("signInDiv").hidden = false;
   }
 
+  function createDriveFile() { 
+    tokenClient.requestAccessToken();
+  }
+
   useEffect(() => {
     /* global google */
+    // const google = window.google; 
     google.accounts.id.initialize({
-      client_id: "191000969607-0cgqepb1p3act7mm5bnbqm8mcl0tafbg.apps.googleusercontent.com",
+      client_id: CLIENT_ID,
       callback: handleCallbackResponse
     });
 
@@ -34,6 +41,36 @@ function GoogleAuth() {
       document.getElementById("signInDiv"),
       {theme: "outline", size: "large"}
     );
+    
+    // Access Tokens
+    // Upload to a specific users google drive
+
+    // tokenClient
+    setTokenClient(
+      google.accounts.oauth2.initTokenClient({ 
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: (tokenResponse) => {
+        console.log(tokenResponse);
+          // We now have access to a live token to use for ANY google API
+          if (tokenResponse && tokenResponse.access_token) {
+            // Google Drive API, we are talking to it with HTTP
+            // Create a file for a certain user
+            fetch("https://www.googleapis.com/drive/v3/files", {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${tokenResponse.access_token}`
+              },
+              body: JSON.stringify({"name": "Cooper Codes File", "mimeType":"text/plain"})
+            });
+          }
+
+        }
+      })
+    );
+      // tokenClient.requestAccessToken();
+
 
     // should ask as soon as site opens
     google.accounts.id.prompt()
@@ -54,6 +91,7 @@ function GoogleAuth() {
         <div>
           <img src={user.picture}></img>
           <h3>{user.name}</h3>
+          <input type="submit" onClick={createDriveFile} value="Create File" />
         </div>
       }
     </div>
